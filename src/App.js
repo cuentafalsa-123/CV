0cf08d863da32150ce24bc86581d7d4d6909a78a
PP <pablopoggiog@gmail.com>
2019-11-27 12:02:15 -0300

    a ver si arreglo

--- src/App.js
+++ src/App.js
@@ -6,9 +6,7 @@ import Experiencia from './components/Experiencia';
 import Navbar from './components/Navbar';
 import './App.css';
 
-
 function App() {
-  
   return (
     <div className='todo'>
     <div className="App">
