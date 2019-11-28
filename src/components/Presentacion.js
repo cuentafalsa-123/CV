46de1947ce16096daf846336caf6cf37bb640200
pablopoggiog <pablopoggiog@gmail.com>
2019-11-27 23:32:30 -0300

    Salvo Presentacion.js

--- src/components/Presentacion.js
+++ src/components/Presentacion.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import './Presentacion.css';
 
-const texto = 'Soy un programador. El Duki del Web Development. Pagame.'
+const texto = 'Lorem ipsum dolor sit amet consectetur adipiscing elit eget platea, ridiculus cum nunc dignissim suspendisse taciti himenaeos sem ut, commodo tempus erat augue urna penatibus sapien mauris. Vivamus tempor quis elementum aenean venenatis molestie litora vehicula, natoque cras lacinia ac phasellus dictum commodo, vel nunc purus facilisi posuere senectus enim. Condimentum felis pulvinar augue dignissim nullam cursus imperdiet sollicitudin tempus id lacinia lobortis hendrerit venenatis aptent, hac posuere consequat duis potenti mattis mi tortor rutrum blandit magna class commodo.'
 
 const Presentacion = () => {
     return (
         <div>
             <p className='texto'>{texto}</p>
-            <p className='texto'>{texto}</p>
-            <p className='texto'>{texto}</p> 
         </div>
     )
 }
