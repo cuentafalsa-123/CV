dee914a950bff625fafb97e22eb5411b97437af1
PP 15.6 <pablopoggiog@gmail.com>
2019-12-30 14:48:58 -0300

    Jeje tuve un error boludo en el string de la conexion, vuelvo a probar

--- database.js
+++ database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const URI = 'mongodb://pablopoggiog:<merecago1512>@pp-shard-00-00-dkxhb.gcp.mongodb.net:27017,pp-shard-00-01-dkxhb.gcp.mongodb.net:27017,pp-shard-00-02-dkxhb.gcp.mongodb.net:27017/test?ssl=true&replicaSet=pp-shard-0&authSource=admin&retryWrites=true&w=majority'
+const URI = 'mongodb://pablopoggiog:merecago1512@pp-shard-00-00-dkxhb.gcp.mongodb.net:27017,pp-shard-00-01-dkxhb.gcp.mongodb.net:27017,pp-shard-00-02-dkxhb.gcp.mongodb.net:27017/test?ssl=true&replicaSet=pp-shard-0&authSource=admin&retryWrites=true&w=majority'
 
 mongoose.connect(URI, 
     {useUnifiedTopology: true,
